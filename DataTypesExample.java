25c8bec3b34e93ffb5173918e9783d58940b1635
Shridhar Borule <109202750+shridharborule512@users.noreply.github.com>
2026-02-24 08:35:49 +0530

    Create DataTypesExample.java

---
+++ DataTypesExample.java
@@ -0,0 +1,30 @@
+public class DataTypesExample {
+
+    public static void main(String[] args) {
+
+        // Integer types
+        int age = 20;
+        long population = 500000000L;
+
+        // Decimal types
+        float price = 99.99f;
+        double pi = 3.1415926535;
+
+        // Character
+        char grade = 'S';
+
+        // Boolean
+        boolean isJavaEasy = true;
+
+        // String (Non-primitive)
+        String name = "Shri";
+
+        System.out.println("Age: " + age);
+        System.out.println("Population: " + population);
+        System.out.println("Price: " + price);
+        System.out.println("Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Easy? " + isJavaEasy);
+        System.out.println("Name: " + name);
+    }
+}
